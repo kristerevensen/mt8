36995f68fd603704cb4d5b31b2f684ff94b430de
kristerevensen <67630525+kristerevensen@users.noreply.github.com>
2024-09-02 00:43:03 +0200

    020924

--- database/migrations/2024_09_01_223619_create_conversions_table.php
+++ database/migrations/2024_09_01_223619_create_conversions_table.php
@@ -16,16 +16,14 @@ class CreateConversionsTable extends Migration
         Schema::create('conversions', function (Blueprint $table) {
             $table->id();
             $table->string('session_id')->index();
-            $table->string('project_code')->index();
-            $table->string('conversion_type');
-            $table->decimal('conversion_value', 10, 2)->nullable();
+            $table->string('goal_uuid')->index();
             $table->string('page_url');
             $table->string('referrer')->nullable();
             $table->timestamp('timestamp')->useCurrent();
             $table->timestamps();
 
-            // Foreign key constraint if project_code relates to a projects table
-            $table->foreign('project_code')->references('project_code')->on('projects')->onDelete('cascade');
+            // Foreign key constraint if goal_uuid relates to a goals table
+            $table->foreign('goal_uuid')->references('goal_uuid')->on('goals')->onDelete('cascade');
         });
     }
 
